d4fb647f8496c22d46758dd3e39b3e7dd547e9b5
Alexandre Terrien <terrienale@eisti.eu>
2018-07-28 11:31:02 +0200

    Add bepo-centric layout for viterbi

---
+++ keyboards/viterbi/keymaps/bepo/keymap.c
@@ -0,0 +1,124 @@
+#include "viterbi.h"
+#include "action_layer.h"
+#include "eeconfig.h"
+
+extern keymap_config_t keymap_config;
+
+#define _BEPO 0 
+#define _LOWER 1
+#define _RAISE 2
+#define _ADJUST 16
+
+enum custom_keycodes {
+  QWERTY = SAFE_RANGE,
+  LOWER,
+  RAISE,
+  ADJUST,
+};
+
+// Fillers to make layering more clear
+#define KC_ KC_TRNS
+#define _______ KC_TRNS
+#define XXXXXXX KC_NO
+#define KC_AJST ADJUST
+#define KC_LOWR LOWER
+#define KC_RASE RAISE
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+[_BEPO] = LAYOUT_kc(
+  //,----+----+----+----+----+----+----.    ,----+----+----+----+----+----+----.
+     VOLU,GRV , 1  , 2  , 3  , 4  , 5  ,      6  , 7  , 8  , 9  , 0  ,MINS,EQL ,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     VOLD,TAB , Q  , W  , E  , R  , T  ,      Y  , U  , I  , O  , P  ,LBRC,RBRC,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     MPLY,ESC , A  , S  , D  , F  , G  ,      H  , J  , K  , L  ,SCLN,QUOT,BSLS,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     MFFD,LSFT, Z  , X  , C  , V  , B  ,      N  , M  ,COMM,DOT ,SLSH,RSFT,PSCR,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     MRWD,LOWR,LCTL,LGUI,LALT,LOWR,SPC ,     ENT ,BSPC,RALT,RASE,RCTL,DEL ,MUTE
+  //`----+----+----+----+----+----+----'    `----+----+----+----+----+----+----'
+),
+[_LOWER] = LAYOUT_kc(
+  //,----+----+----+----+----+----+----.    ,----+----+----+----+----+----+----.
+         ,TILD, F1 , F2 , F3 , F4 , F5 ,      F6 , F7 , F8 , F9 ,F10 ,F11 ,F12 ,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     UNDS,    ,EXLM, AT ,HASH,DLR ,PERC,     HOME,PGDN,PGUP,END ,    ,LCBR,RCBR,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     PLUS,    , F1 , F2 , F3 , F4 , F5 ,     LEFT,DOWN, UP ,RGHT,RCBR,    ,    ,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+         ,    , F7 , F8 , F9 ,F10 ,F11 ,     F12 ,    ,    ,    ,    ,    ,    ,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,    ,         ,DEL ,MNXT,VOLD,VOLU,MPLY,MUTE
+  //`----+----+----+----+----+----+----'    `----+----+----+----+----+----+----'
+
+),
+  [_RAISE] = LAYOUT_kc(
+  //,----+----+----+----+----+----+----.    ,----+----+----+----+----+----+----.
+         ,TILD, F1 , F2 , F3 , F4 , F5 ,      F6 , F7 , F8 , F9 ,F10 ,F11 ,F12 ,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     UNDS,    , 1  , 2  , 3  , 4  , 5  ,      6  , 7  , 8  , 9  , 0  ,LCBR,RCBR,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+     PLUS,    , F1 , F2 , F3 , F4 , F5 ,      F6 ,MINS,EQL ,LBRC,RBRC,    ,    ,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+         ,    , F7 , F8 , F9 ,F10 ,F11 ,     F12 ,NUHS,NUBS,    ,    ,    ,    ,
+  //|----+----+----+----+----+----+----|    |----+----+----+----+----+----+----|
+         ,    ,    ,    ,    ,    ,    ,         ,    ,MNXT,VOLD,VOLU,MPLY,MUTE
+  //`----+----+----+----+----+----+----'    `----+----+----+----+----+----+----'
+  ),
+
+  [_ADJUST] = LAYOUT(
+    _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  _______, \
+    _______, _______, RESET  , RGB_TOG, RGB_MOD, RGB_HUD, RGB_HUI, RGB_SAD, RGB_SAI, RGB_VAD, RGB_VAI, _______, KC_DEL,  _______, \
+    _______, _______, _______, _______, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP, QWERTY,  _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, \
+    _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______ \
+  )
+};
+
+
+void persistent_default_layer_set(uint16_t default_layer) {
+  eeconfig_update_default_layer(default_layer);
+  default_layer_set(default_layer);
+}
+
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  switch (keycode) {
+    case QWERTY:
+      if (record->event.pressed) {
+        #ifdef AUDIO_ENABLE
+          PLAY_NOTE_ARRAY(tone_qwerty, false, 0);
+        #endif
+        persistent_default_layer_set(1UL<<_BEPO);
+      }
+      return false;
+      break;
+    case LOWER:
+      if (record->event.pressed) {
+        layer_on(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_LOWER);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case RAISE:
+      if (record->event.pressed) {
+        layer_on(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      } else {
+        layer_off(_RAISE);
+        update_tri_layer(_LOWER, _RAISE, _ADJUST);
+      }
+      return false;
+      break;
+    case ADJUST:
+      if (record->event.pressed) {
+        layer_on(_ADJUST);
+      } else {
+        layer_off(_ADJUST);
+      }
+      return false;
+      break;
+  }
+  return true;
+}
